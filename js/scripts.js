dd6a0e12fe5e4e1a912f034fb690db8d7baf3975
jh and md <md@>
2018-06-07 10:32:52 -0700

    Add function to check validity of numbers and limits of input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
-$(document).ready(function)({
-
-
+$(document).ready(function(){
+  var input = prompt("Convert any number below 3,999 to roman numerals");
+  //checkInputValidity(input);
 });
 
-function
-
-
-if ("input" === NaN) {
-  "invalid input"
-} else if ("input" > 3,999) {
-  "invalid input"
-} else if ( input === 1 || input === 5 || input === 10 || input === 50 || input === 100 || input === 500 || input === 1000 ) {
-  
-} else if (array.length > # && "input" > #) {
-
+function checkInputValidity(input){
+  //Check for invalid entries
+  if (isNaN(input)) {
+    alert("Invalid input");
+  } else if (input > 3999) {
+    alert("Invalid input");
+  } else {
+    alert("Valid Number");
+  }
 }
+
+
+//if ("input" === NaN) {
+//  "invalid input"
+//} else if ("input" > 3,999) {
+//  "invalid input"
+//} else if ( input === 1 || input === 5 || input === 10 || input === 50 || input === 100 || input === 500 || input === 1000 ) {
+//}
+//} else if (array.length > # && "input" > #) {
+
+//}
